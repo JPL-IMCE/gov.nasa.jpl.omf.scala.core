e33e585bbd0c24119fda8feb3f7f887b46a90cfe
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-20 19:34:49 -0700

    MBEE-198, SSCAES-1326: Simpler SBT project structure => no SBT NPE!

--- project/OMFCore.scala
+++ project/OMFCore.scala
@@ -24,86 +24,17 @@ object OMFCore extends Build {
         case Some(dir) => Some(Resolver.file("file", new File(dir))(Resolver.ivyStylePatterns))
         case None => sys.error("Set -DJPL_MBEE_LOCAL_REPOSITORY=<dir> where <dir> is a local Ivy repository directory")
       }
+    },
+    resolvers += {
+      Option.apply(System.getProperty("JPL_MBEE_LOCAL_REPOSITORY")) match {
+        case Some(dir) => Resolver.file("file", new File(dir))(Resolver.ivyStylePatterns)
+        case None => sys.error("Set -DJPL_MBEE_LOCAL_REPOSITORY=<dir> where <dir> is a local Ivy repository directory")
+      }
     }
   )
 
   lazy val commonSettings =
     Defaults.coreDefaultSettings ++ Defaults.runnerSettings ++ Defaults.baseTasks ++ graphSettings
-
-  lazy val aggregateDependenciesPublishSettings = Seq(
-    // disable publishing the main jar produced by `package`
-    publishArtifact in(Compile, packageBin) := false,
-
-    // disable publishing the main API jar
-    publishArtifact in(Compile, packageDoc) := false,
-
-    // disable publishing the main sources jar
-    publishArtifact in(Compile, packageSrc) := false
-  )
-
-  lazy val dependenciesPackSettings = packSettings ++ Seq(
-    packExpandedClasspath := true,
-    packLibJars := Seq.empty,
-    packExcludeArtifactTypes := Seq("src", "doc"),
-    (mappings in pack) := {
-      extraPackFun.value
-    }
-  ) ++ publishPackZipArchive
-
-  val coreLibs = Project(
-    "coreLibs",
-    file("coreLibs")).
-    settings(versionWithGit: _*).
-    settings(jplSettings: _*).
-    settings(commonSettings: _*).
-    settings(aggregateDependenciesPublishSettings: _*).
-    settings(dependenciesPackSettings: _*).
-    settings(
-      libraryDependencies ++= Seq(
-        "org.scala-lang" % "scala-reflect" % Versions.scala % "compile" withSources() withJavadoc(),
-        "org.scala-lang" % "scala-library" % Versions.scala % "compile" withSources() withJavadoc(),
-        "org.scala-lang" % "scala-compiler" % Versions.scala % "compile" withSources() withJavadoc(),
-        "org.scala-lang.modules" %% "scala-xml" % Versions.scala_xml % "compile" withSources() withJavadoc(),
-        "org.scala-lang.modules" %% "scala-parser-combinators" % Versions.scala_parser_combinators % "compile" withSources() withJavadoc(),
-        "org.scalacheck" %% "scalacheck" % Versions.scalaCheck % "test" withSources() withJavadoc(),
-        "org.scalatest" %% "scalatest" % Versions.scalaTest withSources() withJavadoc(),
-        "org.specs2" %% "specs2-core" % Versions.specs2 % "compile" withSources() withJavadoc(),
-        "org.scalaz.stream" %% "scalaz-stream" % Versions.scalaz_stream % "compile" withSources() withJavadoc(),
-        "org.scalaz" %% "scalaz-core" % Versions.scalaz % "compile" withSources() withJavadoc(),
-        "org.scalaz" %% "scalaz-effect" % Versions.scalaz % "compile" withSources() withJavadoc()
-      )
-    )
-
-  val playLibs = Project(
-    "playLibs",
-    file("playLibs")).
-    settings(versionWithGit: _*).
-    settings(jplSettings: _*).
-    settings(commonSettings: _*).
-    settings(aggregateDependenciesPublishSettings: _*).
-    settings(dependenciesPackSettings: _*).
-    settings(
-      libraryDependencies ++= Seq(
-        "com.typesafe.play" %% "play" % Versions.play % "compile" withSources(),
-        "com.typesafe.play" %% "play-iteratees" % Versions.play % "compile" withSources(),
-        "com.typesafe.play" %% "play-json" % Versions.play % "compile" withSources(),
-        "com.typesafe.play" %% "play-functional" % Versions.play % "compile" withSources()
-      ),
-      libraryDependencies ~= {
-        _ map {
-          case m if m.organization == "com.typesafe.play" =>
-            m.
-              exclude("commons-codec", "commons-codec").
-              exclude("commons-logging", "commons-logging").
-              exclude("com.typesafe", "config").
-              exclude("com.typesafe.play", "sbt-link").
-              exclude("org.slf4j", "slf4j-api").
-              exclude("org.slf4j", "slf4j-nop")
-          case m => m
-        }
-      }
-    )
-
 
   lazy val sourcePublishSettings = Seq(
     // disable publishing the main jar produced by `package`
@@ -153,52 +84,11 @@ object OMFCore extends Build {
     settings(
       removeExistingHeaderBlock := true,
       scalaSource in Compile := baseDirectory.value / "src",
-      scalaSource in Test := baseDirectory.value / "test"
-    ).
-    dependsOn(coreLibs, playLibs) //enablePlugins( play.PlayScala )
+      scalaSource in Test := baseDirectory.value / "test",
 
-  val extraPackFun: Def.Initialize[Task[Seq[(File, String)]]] = Def.task[Seq[(File, String)]] {
-    def getFileIfExists(f: File, where: String): Option[(File, String)] = if (f.exists()) Some((f, s"$where/${f.getName}")) else None
-
-    val ivyHome: File = Classpaths.bootIvyHome(appConfiguration.value) getOrElse sys.error("Launcher did not provide the Ivy home directory.")
-
-    // this is a workaround; how should it be done properly in sbt?
-
-    // goal: process the list of library dependencies of the project.
-    // that is, we should be able to tell the classification of each library dependency module as shown in sbt:
-    //
-    // > show libraryDependencies
-    // [info] List(
-    //    org.scala-lang:scala-library:2.11.2, 
-    //    org.scala-lang:scala-library:2.11.2:provided, 
-    //    org.scala-lang:scala-compiler:2.11.2:provided, 
-    //    org.scala-lang:scala-reflect:2.11.2:provided, 
-    //    com.typesafe:config:1.2.1:compile, 
-    //    org.scalacheck:scalacheck:1.11.5:compile, 
-    //    org.scalatest:scalatest:2.2.1:compile, 
-    //    org.specs2:specs2:2.4:compile, 
-    //    org.parboiled:parboiled:2.0.0:compile)
-
-    // but... libraryDependencies is a SettingKey (see ld below)
-    // I haven't figured out how to get the sequence of modules from it.
-    //val ld: SettingKey[Seq[ModuleID]] = libraryDependencies
-
-    // workaround... I found this API that I managed to call...
-    // this overrides the classification of all jars -- i.e., it is as if all library dependencies had been classified as "compile".
-
-    // for now... it's a reasonable approaximation of the goal...
-    val managed: Classpath = Classpaths.managedJars(Compile, classpathTypes.value, update.value)
-    val result: Seq[(File, String)] = managed flatMap { af: Attributed[File] =>
-      af.metadata.entries.toList flatMap { e: AttributeEntry[_] =>
-        e.value match {
-          case null => Seq()
-          case m: ModuleID => Seq() ++
-            getFileIfExists(new File(ivyHome, s"cache/${m.organization}/${m.name}/srcs/${m.name}-${m.revision}-sources.jar"), "lib.srcs") ++
-            getFileIfExists(new File(ivyHome, s"cache/${m.organization}/${m.name}/docs/${m.name}-${m.revision}-javadoc.jar"), "lib.javadoc")
-          case _ => Seq()
-        }
-      }
-    }
-    result
-  }
+      libraryDependencies ++= Seq(
+        "gov.nasa.jpl.mbee" %% "jpl-mbee-common-scala-libraries_core" % Versions.jpl_mbee_core,
+        "gov.nasa.jpl.mbee" %% "jpl-mbee-common-scala-libraries_other" % Versions.jpl_mbee_other
+      )
+    )
 }
--- project/Versions.scala
+++ project/Versions.scala
@@ -1,12 +1,6 @@
 
 object Versions {
   val scala = "2.11.6"
-  val scalaCheck = "1.12.2"
-  val scalaTest = "2.2.4"
-  val specs2 = "3.6"
-  val scalaz_stream = "0.7a"
-  val scalaz = "7.1.1"
-  val play = "2.4.0-RC5"
-  val scala_xml = "1.0.3"
-  val scala_parser_combinators = "1.0.3"
+  val jpl_mbee_core = "1800.02-fe2a87da62a4d394e4307d800a2c77733c8a381d"
+  val jpl_mbee_other = "1800.02-fe2a87da62a4d394e4307d800a2c77733c8a381d"
 }
