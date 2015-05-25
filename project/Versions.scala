6fbb5add7ec5763ea899bc4f46b502073d5c5013
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-25 16:20:21 -0700

    MBEE-209: SSCAES-1323 (add scala-graph libraries)

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val jpl_mbee_release_prefix="1800.02-"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="650c69e6e7defc0b3e430d9c3cb290e8b3cc1f88"
+  val jpl_mbee_common_scala_libraries_revision="9d02e73b6963bff2e63e6c7a90d49198673f6860"
 
   val jpl_mbee_core = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
   val jpl_mbee_other = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
