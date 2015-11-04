1cdc8c3528e3eb7227670d7f8435a16ef8627af5
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-11-04 12:32:25 -0800

    updated libraries

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,6 +2,6 @@
 object Versions {
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="202d290d17805d26522329ceb8c09fe5945ffebf"
+  val jpl_mbee_common_scala_libraries_revision="d44cf0591f31cb1005a9b4a2fab47bfd235e005c"
 
 }
