1e7be1adc4f4f02ab214d72a55eef0c2f6dba3e3
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-01-08 05:59:56 +0000

    copyright year updated

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project
-  val version = "1.0"
+  val version = "1.1"
 
   // https://github.jpl.nasa.gov/imce/jpl-mbee-common-scala-libraries
   val jpl_mbee_common_scala_libraries="1.3"
--- src/gov/nasa/jpl/omf/scala/core/OMFModule.scala
+++ src/gov/nasa/jpl/omf/scala/core/OMFModule.scala
@@ -2,7 +2,7 @@
  *
  * License Terms
  *
- * Copyright (c) 2014-2015, California Institute of Technology ("Caltech").
+ * Copyright (c) 2014-2016, California Institute of Technology ("Caltech").
  * U.S. Government sponsorship acknowledged.
  *
  * All rights reserved.
