53b4cb90b6a636ee1f978d25633f8b4ae4f5a97d
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-01-09 07:00:36 +0000

    use libraries v1.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,9 +2,9 @@
 object Versions {
 
   // this project
-  val version = "1.1"
+  val version = "1.2"
 
   // https://github.jpl.nasa.gov/imce/jpl-mbee-common-scala-libraries
-  val jpl_mbee_common_scala_libraries="1.3"
+  val jpl_mbee_common_scala_libraries="1.4"
 
 }
