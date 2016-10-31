3ad96cecb7520569fd932cac793ec43e4fffba9a
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-10-31 20:25:10 +0000

    travis-ci

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -5,9 +5,11 @@ set -ev
 # Build if TRAVIS_TAG is unset or empty.
 [ -n "${TRAVIS_TAG}" ] && exit 0;
 
-# Build if there is no tag for the current commit.
+# Get the tag for this commit
 t=$(git name-rev --tags --name-only $(git rev-parse HEAD))
-[ -n "$t" ] && exit 0;
+
+# Bypass the build if the tag is anything but 'undefined'.
+[ "undefined" != "$t" ] && exit 0;
 
 [ ${TRAVIS_SECURE_ENV_VARS} == false ] && exit -1;
 
